8d350d91bc6cfe76a5d5981248edfe6ce45ef326
Dikri Ghaniyyu Rahman <dikrighon@gmail.com>
2024-10-09 22:19:46 +0700

    responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,31 @@
-function convertCelciusToFahrenheit() {
-  var c = document.getElementById("input-suhu").value;
-  var f = c * (9 / 5) + 32;
-  document.getElementById("hasil-suhu").value = f;
+// function convertCelciusToFahrenheit() {
+//   var c = document.getElementById("input-suhu").value;
+//   var f = c * (9 / 5) + 32;
+//   document.getElementById("hasil-suhu").value = f;
+// }
+console.log('tersambung');
+
+function validateForm(){
+  console.log('Validasi tersambung');
+  const inputSuhu = document.getElementById('input-suhu').value;
+
+  if (inputSuhu == ''){
+    alert('input anda kosong');
+  } else {
+    calculate(inputSuhu);
+  }
+}
+
+function calculate(value){
+let result = parseInt(value) * (9/5) +32;
+document.getElementById('hasil-suhu').value = result;
+document.getElementById('cara-kalkulasi').value = value + ' x (9/5) + 32 = ' + result;
+}
+
+
+function reset() {
+  document.getElementById('input-suhu').value = '';
+  document.getElementById('hasil-suhu').value = '';
+  document.getElementById('cara-kalkulasi').value = '';
+
 }
