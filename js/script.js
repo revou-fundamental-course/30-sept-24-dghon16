f1b95ac820958452ae8a2dad043a29870e23d000
Dikri Ghaniyyu Rahman <dikrighon@gmail.com>
2024-10-09 15:40:26 +0700

    commit update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-// scipt
+function convertCelciusToFahrenheit() {
+  var c = document.getElementById("input-suhu").value;
+  var f = c * (9 / 5) + 32;
+  document.getElementById("hasil-suhu").value = f;
+}
